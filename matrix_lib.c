b0c5668f0a34c088424ea35da9f1f4940cbafe2a
diegomirandap <diegomirandapinto2003@gmail.com>
2025-09-11 11:07:09 -0300

    pronto para enviar

--- matrix_lib.c
+++ matrix_lib.c
@@ -16,12 +16,12 @@ int matrix_matrix_mult(struct matrix* matrixA, struct matrix* matrixB, struct ma
     if (matrixA->width != matrixB->height || matrixC->height != matrixA->height || matrixC->width != matrixB->width) 
         return 0;
 
-    // Verificar se é necessário forçar a inicialização como 0 (dentro ou fora do alg), ou se ele já está realmente gerando como 0
-
     for(unsigned long int i = 0; i < matrixA->height; i++){
+        //inicializar o vetor C como 0
+        for (unsigned long int j = 0; j < matrixC->width; j++) {
+            matrixC->rows[i * matrixC->width + j] = 0.0f;
+        }
         for(unsigned long int k = 0; k < matrixA->width; k++){  
-            //inicializar o vetor C como 0
-            matrixC->rows[i * matrixC->width + k] = 0;
             for (unsigned long int j = 0; j < matrixB->width; j++){
                 // acesso final por linha de B               
                 // casa de C += casa Aik * cada um da linha de B
