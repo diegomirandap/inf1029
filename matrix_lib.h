8c2e68fba97614b90627540a657c8a8464ff0332
diegomirandap <diegomirandapinto2003@gmail.com>
2025-11-18 09:34:51 -0300

    fix .h

--- matrix_lib.h
+++ matrix_lib.h
@@ -1,15 +1,3 @@
-//INF-1029 - Trabalho 3
-//Diego Miranda - 2210996
-//Felipe Cancella - 2210487
-typedef struct matrix {
-    unsigned long int height;
-    unsigned long int width;
-    float *rows;
-}matrix;
-
-int scalar_matrix_mult(float scalar_value, struct matrix* matrix);
-int matrix_matrix_mult(struct matrix* matrixA, struct matrix* matrixB, struct matrix* matrixC);
-void set_number_threads(int num_threads);
 //INF-1029 - Trabalho 4
 //Diego Miranda - 2210996
 //Felipe Cancella - 2210487
